5cbb4662383b868583ed252f4891fab6f9962591
ErmannoBartoli <ermannino@gmail.com>
2021-06-21 11:00:25 +0200

    aggiunte

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 import * as THREE from './libs/threejs/build/three.module.js';
 import { OrbitControls } from './libs/threejs/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from './libs/threejs/examples/jsm/loaders/GLTFLoader.js';
+import TWEEN from './libs/tween.esm.js';
 
 var scene;
+var frames = 0;
+var objectsTween;
+var vehicles;
 
 var config = {
   game: {
     yspawn: 0,
+    zspawn: 0,
+    x_lane_0: -7.3,
+    x_lane_1: -2.4,
+    x_lane_2: 2.4,
+    x_lane_3: 7.3,
+    z_lane: -40,
   }
 }
 
 //Declaring the car
 var ferrari = {
-  mesh: new THREE.Object3D()
+  mesh: new THREE.Object3D(),
+  positions :{
+    left: -1,
+    right: 1,
+  }
 }
 var road = {
   mesh: new THREE.Object3D()
@@ -21,6 +35,7 @@ var road = {
 const models = {
   ferrari: {url: "./assets/cars/ferrari/scene.gltf"},
   road: {url: "./assets/environment/road/scene.gltf"},
+  truck: {url: "./assets/cars/truck/scene.gltf"},
 }
 
 
@@ -46,7 +61,26 @@ function initRoad(){
   cube.position.set(0, config.game.yspawn - 0.5 , 0);
   cube.rotation.set(0,Math.PI/2,0);
   cube.scale.set(150,1,20);
+  cube.receiveShadow = true;
   scene.add( cube );
+}
+
+function spawnTruck(){
+  var truck = new THREE.Object3D();
+  truck.name = "Truck";
+  let body = models.truck.gltf.clone();
+  truck.add(body);
+  truck.rotation.y = -Math.PI;
+  truck.position.set(7.3, 0, -40);
+  truck.scale.set(0.04,0.04,0.04);
+
+  //scene.add(truck);
+  vehicles.add(truck);
+}
+
+function initvehicles(){
+  vehicles = new THREE.Group();
+  scene.add(vehicles);
 }
 
 
@@ -108,6 +142,8 @@ function init(){
   //Set up of the renderer
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFShadowMap;
   document.body.appendChild( renderer.domElement );
 
 
@@ -115,26 +151,117 @@ function init(){
   const controls = new OrbitControls(camera, renderer.domElement);
   
   
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+  //Setting the Lights
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 3, 100);
+  directionalLight.position.set(0, 10, config.game.zspawn + 3);
+  var directionalLightTargetObject = new THREE.Object3D();
+  directionalLightTargetObject.position.set(0, 0, config.game.zspawn + 4);
+
   scene.add(ambientLight);
-  //const directionalLight = new THREE.DirectionalLight(0xffffff, 3, 100);
-  //directionalLight.position.set(0,0,10);
-  //scene.add(directionalLight);
-  const hemisphereLight = new THREE.HemisphereLight(0xffffff,0.1, 10);
-  hemisphereLight.position.set(0,0,10);
-  scene.add(hemisphereLight);
+  scene.add(directionalLightTargetObject);
+
+  directionalLight.target = directionalLightTargetObject;
+  directionalLight.castShadow = true;
+
+  directionalLight.shadow.camera.left = -100;
+	directionalLight.shadow.camera.right = 10;
+	directionalLight.shadow.camera.top = 140;
+	directionalLight.shadow.camera.bottom = 0;
+	directionalLight.shadow.camera.near = 30;
+	directionalLight.shadow.camera.far = 5;
+	directionalLight.shadow.bias = 0.0009;
+
+  scene.add(directionalLight);
 
 
   
   initFerrari();
   initRoad();
-
+  initvehicles();
+  spawnTruck();
+  initListenerKeyboard();
+  moveVehicles();
   const animate = function() {
     requestAnimationFrame( animate );
+    frames += 1;
+    TWEEN.update();
     renderer.render( scene, camera );
   }
   animate();
 
 }
 
+// SETTING THE LISTENER FOR THE ANIMATIONS
 
+function initListenerKeyboard(){
+  document.onkeydown = function(e){
+    switch(e.code){
+      case 'KeyA':
+      case 'ArrowLeft':
+        moveLeft();
+        break;
+      case 'KeyD':
+      case 'ArrowRight':
+        moveRight();
+    }
+  }
+}
+
+function moveLeft(){
+  if (ferrari.mesh.position.x < config.game.x_lane_0) return;
+  performMovementTo(ferrari.positions.left);
+}
+function moveRight(){
+  if (ferrari.mesh.position.x > config.game.x_lane_3) return;
+  performMovementTo(ferrari.positions.right);
+}
+
+function performMovementTo( pos){
+  var delta = { x: 0 };
+	if (pos < 0){
+    objectsTween = new TWEEN.Tween(delta)
+	.to({ x: -0.1 },10) 
+	.easing(TWEEN.Easing.Linear.None)
+	.onUpdate( 
+				() => {
+					ferrari.mesh.position.x = ferrari.mesh.position.x + delta.x;
+				}
+	).onComplete(
+				() => {
+					moveVehicles();
+				}
+	).start();
+  }else{
+    objectsTween = new TWEEN.Tween(delta)
+	.to({ x: 0.1 },10) 
+	.easing(TWEEN.Easing.Linear.None)
+	.onUpdate( 
+				() => {
+					ferrari.mesh.position.x = ferrari.mesh.position.x + delta.x;
+				}
+	).onComplete(
+				() => {
+					moveVehicles();
+				}
+	).start();
+  }
+}
+
+function moveVehicles(){
+  
+	// init tween
+	var delta = { z: 0 };
+	objectsTween = new TWEEN.Tween(delta)
+	.to({ z: 0.1 },1) 
+	.easing(TWEEN.Easing.Linear.None)
+	.onUpdate( 
+				() => {
+					vehicles.position.z = vehicles.position.z + delta.z;
+				}
+	).onComplete(
+				() => {
+					moveVehicles();
+				}
+	).start();
+}
